e38edcd1989aa00f26a7e2be1a5fa23280578188
Hunter H <hunter.hester15@gmail.com>
2023-04-16 18:36:28 -0500

    Adds userPrompt logic with error handling

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
-const inquirer = require('inquirer');
+const promptUser = require('./lib/userPrompt.js');
 
-//prompt user for questions regarding logo creation
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'initials',
-            message: 'Enter initials for logo:',
-            validate: value => value.length > 3 ? 'Initials must contain no more than 3 characters' : true
-        },
-        {
-            type: 'input',
-            name: 'textcolor',
-            message: 'Enter a color keyword (OR a hexadecimal number) for logo text color:'
-            //requires regex validation
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape:',
-            choices: [
-                'circle',
-                'triangle',
-                'square'
-            ]
-        },
-        {
-            type: 'input',
-            name: 'shapecolor',
-            message: 'Enter a color keyword (OR a hexadecimal number) for shape color:'
-            //requires regex validation
-        }
-    ])
 
-    .then((answers) => {
-        console.log(answers)
-    })
+const init = () => {
+    promptUser()
 }
 
-promptUser()
+init();
