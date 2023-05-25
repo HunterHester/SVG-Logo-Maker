13742b5661c65853e87003c58f81cec5c4ea5e1a
Hunter H <hunter.hester15@gmail.com>
2023-05-25 13:42:44 -0500

    adds completed code to create SVG using prompt

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const promptUser = require('./lib/userPrompt.js');
+const generateShape = require('./lib/generateShape.js');
+const fs = require('fs');
+const file = "./examples/example.svg"
+
+function createFile(response) {
+    const svg = generateShape(response);
+    console.log('generating shape and creating file');
+    console.log(svg)
+    fs.writeFile(file, svg, () => console.log('Generated SVG file.'));
+}
 
 
 const init = () => {
     promptUser()
+        .then((response) => {
+            console.log("input check");
+            createFile(response)
+        })
+        .catch(err => {
+            console.log(err)
+        });
 }
 
 init();
